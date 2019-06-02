705ae243ab1d6a169ec4c25cd0a1c9c55236ae13
Chad Ohl <chadohl@gmail.com>
2019-06-02 15:17:47 -0700

    Initial commit, adding files

---
+++ config.js
@@ -0,0 +1,7 @@
+/** swap out */
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/81a716fa-0fe1-433e-8f2f-f8c5c53d45b0/token";
+
+/** swap out */
+const instanceLocator = "v1:us1:81a716fa-0fe1-433e-8f2f-f8c5c53d45b0";
+
+export { tokenUrl, instanceLocator }
